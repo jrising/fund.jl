bbb2dbe46c806b15d8c465d918c3deb725b69c1c
James Rising <jarising@gmail.com>
2015-11-01 20:22:16 -0800

    macro estimate

---
+++ src/ImpactedLossComponent.jl
@@ -0,0 +1,20 @@
+﻿using Mimi
+
+@defcomp impactedloss begin
+    regions = Index()
+
+    loss = Variable(index=[time,regions])
+
+    income = Parameter(index=[time, regions])
+    impactedincome = Parameter(index=[time, regions])
+end
+
+function timestep(s::impactedloss, t::Int)
+    v = s.Variables
+    p = s.Parameters
+    d = s.Dimensions
+
+    for r in d.regions
+        v.loss[t, r] = -(p.impactedincome[t, r] - p.income[t, r]) * 1e9
+    end
+end
---
+++ src/TemperatureMacroeconomicComponent.jl
@@ -0,0 +1,29 @@
+﻿using Mimi
+
+@defcomp temperaturemacroeconomic begin
+    regions = Index()
+
+    baseline = Variable(index=[regions])
+    changeypcgrowth = Variable(index=[time,regions])
+
+    macrocoeffs::Vector{Float64} = Parameter()
+    temp90 = Parameter(index=[regions])
+    regtmp = Parameter(index=[time,regions])
+end
+
+function timestep(s::temperaturemacroeconomic, t::Int)
+    v = s.Variables
+    p = s.Parameters
+    d = s.Dimensions
+
+    if t == 1
+        for r in d.regions
+            v.changeypcgrowth[t, r] = 1
+            v.baseline[r] = sum(p.macrocoeffs .* [p.regtmp[1, r], p.regtmp[1, r] * p.temp90[r]])
+        end
+    else
+        for r in d.regions
+            v.changeypcgrowth[t, r] = exp(sum(p.macrocoeffs .* [p.regtmp[t, r], p.regtmp[t, r] * p.temp90[r]]) - v.baseline[r])
+        end
+    end
+end
