e87bf123aa1b40bd44189757700b88a9e1684e37
James Rising <jarising@gmail.com>
2015-11-02 19:31:36 -0800

    updated to simple quadratic

--- src/TemperatureMacroeconomicComponent.jl
+++ src/TemperatureMacroeconomicComponent.jl
@@ -19,11 +19,11 @@ function timestep(s::temperaturemacroeconomic, t::Int)
     if t == 1
         for r in d.regions
             v.changeypcgrowth[t, r] = 1
-            v.baseline[r] = sum(p.macrocoeffs .* [p.regtmp[1, r], p.regtmp[1, r] * p.temp90[r]])
+            v.baseline[r] = sum(p.macrocoeffs .* [p.regtmp[1, r] + p.temp90[r], (p.regtmp[1, r] + p.temp90[r])^2])
         end
     else
         for r in d.regions
-            v.changeypcgrowth[t, r] = exp(sum(p.macrocoeffs .* [p.regtmp[t, r], p.regtmp[t, r] * p.temp90[r]]) - v.baseline[r])
+            v.changeypcgrowth[t, r] = exp(sum(p.macrocoeffs .* [p.regtmp[t, r] + p.temp90[r], (p.regtmp[t, r] + p.temp90[r])^2]) - v.baseline[r])
         end
     end
 end
